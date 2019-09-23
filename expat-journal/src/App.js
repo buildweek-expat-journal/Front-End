2f4b0ca851bb95af525604753b93d88884ccdc48
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-09-23 16:53:54 +0100

    complete draft of Login form with Formik - includes basic styling

--- expat-journal/src/App.js
+++ expat-journal/src/App.js
@@ -1,10 +1,37 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import styled from "styled-components";
+import LoginForm from "./LoginForm";
+import "./styles.css";
+
+const StyledLogin = styled.div`
+  width: 500px;
+  border: 1px solid black;
+  margin: 0 auto;
+  height: 400px;
+  background-color: #f7f7f7;
+  font-family: "Roboto", sans-serif;
+  text-align: left;
+  padding-left: 20px;
+  box-shadow: 10px 10px 16px 0px rgba(0, 0, 0, 0.75);
+
+  h1 {
+    padding-bottom: 30px;
+    text-align: center;
+  }
+
+  label {
+    padding-left: 10px;
+  }
+`;
 
 function App() {
   return (
     <div className="App">
-
+      <StyledLogin>
+        <h1>Login</h1>
+        <LoginForm />
+      </StyledLogin>
     </div>
   );
 }
