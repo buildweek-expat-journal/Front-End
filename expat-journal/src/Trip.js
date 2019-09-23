9164bc06c8c1f7a355652acf287bacb4deafc6f2
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-09-23 16:09:00 -0400

    Adds blog and pictures NavLinks

--- expat-journal/src/Trip.js
+++ expat-journal/src/Trip.js
@@ -1,27 +1,33 @@
 import React from 'react';
-import {Router, NavLink} from 'react-router-dom';
+import { Route, NavLink } from 'react-router-dom';
+import Blog from './Blog'
 
 
 function Trip(props) {
     return (
         <div className='trip-wrapper'>
             {/* header with the location of the trip */}
-            <h1>{`location goes here`}</h1>
+            <h1>{`trip location goes here`}</h1>
             {/* banner image of the location */}
-            <img src=""></img>
+            <img src='https://images.unsplash.com/photo-1567913300214-364d5256df1c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80'></img>
+            <p>placeholder image</p>
 
+            {/* bottom section of page underneath the banner image that will display the tabs */}
             <nav className='trip-sub-nav'>
-                {/* links to a Blog tab with a description of that given trip */}
-                <NavLink to={`/traveler/location/${trip.id}`}>Blog</NavLink>
+                {/*  Blog tab with a description of that given trip */}
+                <NavLink to={`/traveler/location/:id/`}>Blog</NavLink>
                 {/* links to a Pictures tab with an image grid of a given trip */}
-                <NavLink to={`/traveler/location/${trip.id}`}>Pictures</NavLink>
+                <NavLink to={`/traveler/location/:id/`}>Pictures</NavLink>
             </nav>
 
             {/* renders the blog text/description of the trip */}
             <Route 
-                exact path="/traveler/location/:id"
-                render={() => <Blog trip={trip} /> }
+                exact path="/traveler/location/:id/"
+                render={() => <Blog /> }
             />
+            {/* routes to images */}
+            <Route 
+                exact path="/traveler ..." />
             
         </div>
     )
