2ca051de5db49bcd20e5ac31212480cc7b247f09
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-09-23 13:22:44 -0400

    Creates branch and Trip component

--- expat-journal/src/App.js
+++ expat-journal/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Trip from './Trip';
 
 function App() {
   return (
@@ -8,14 +9,14 @@ function App() {
       <div className="App">
         <header></header>
         <Switch>
-          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/browse" component={Browse} />
-          <Route exact path="/traveler/:id" component={Traveler} />
-          <Route exact path="/traveler/location/:id" component={Trip} />
-          <PrivateRoute path="/bubbles" component={Profile} />
+          <Route exact path="/traveler/:id" component={Traveler} /> */}
+          <Route exact path="/traveler/location/" component={Trip} />
+          {/* <PrivateRoute path="/bubbles" component={Profile} />
           <PrivateRoute path="/bubbles" component={EditTrip} />
-          <PrivateRoute path="/bubbles" component={PrivateTrip} />
+          <PrivateRoute path="/bubbles" component={PrivateTrip} /> */}
         </Switch>
       </div>
     </Router>
---
+++ expat-journal/src/Trip.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {Router, NavLink} from 'react-router-dom';
+
+
+function Trip() {
+    return (
+        <div className='trip-wrapper'>
+            <h1>Location</h1>
+            <img src="https://images.unsplash.com/photo-1567913300214-364d5256df1c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1051&q=80"></img>
+
+            <div className='trip-sub-nav'>
+
+            </div>
+
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+export default Trip;
