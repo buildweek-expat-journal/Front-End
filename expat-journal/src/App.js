b00996bbc1523df9b4f84e12d58d69af487e37be
Ryan Ziegenfus <thewebcoderz@gmail.com>
2019-09-24 18:00:54 -0400

    image and blog routes display on public trip component

--- expat-journal/src/App.js
+++ expat-journal/src/App.js
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./index.css";
 import './App.css';
 import Header from './components/Header'
-// import Home from './components/Home'
 import Browse from './components/Browse'
 import Traveler from './components/Traveler'
 import Trip from './components/Trip'
@@ -12,6 +11,7 @@ import LoginForm from "./components/LoginForm";
 // import EditTrip from "./components/EditTrip";
 // import PrivateTrip from "./components/PrivateTrip";
 import PrivateRoute from './auth/routes/PrivateRoute'
+import WelcomePage from './components/WelcomePage'
 
 import {TripContext} from './contexts/TripContext';
 import { ProfileContext } from './contexts/ProfileContext';
@@ -70,20 +70,17 @@ function App() {
       <Router>
         <div className="App">
           <Switch>
-
-            {/* <ProfileContext.Provider value={{travelerState, setTraveler}}>
-              <Route exact path="/" component={Home} />
-            </ProfileContext.Provider> */}
+            <Route exact path="/" component={WelcomePage} />
             <Route exact path="/browse" component={Browse} />
             <PrivateRoute exact path='/profile/:id' component={Profile} />
             <Route exact path="/traveler/:id" component={Traveler} />
-            <Route exact path="/traveler/:id/location/:lid" component={Trip} />
+            <Route path="/traveler/:id/location/:lid" component={Trip} />
             <ProfileContext.Provider value={{loginState, setLogin, setTraveler, travelerState}}>
               <Route exact path="/login" component={LoginForm} />
             </ProfileContext.Provider> 
 
             {/* <PrivateRoute path="/profile/:id/editTrip/:lid" component={EditTrip} />
-            <PrivateRoute path="/profile/:id/myTrip/:lid" component={PrivateTrip} />  */}
+            <PrivateRoute path="/profile/:id/location/:lid" component={PrivateTrip} />  */}
           </Switch>
         </div>
       </Router>
